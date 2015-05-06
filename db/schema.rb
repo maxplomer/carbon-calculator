dfedf841c8b37ec8590a1133e9bb74d235aa5606
Max Plomer <maxplomer@gmail.com>
2015-05-06 14:15:25 -0400

    change to postgres database for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140428042342) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: true do |t|
     t.string   "username",                    null: false
     t.datetime "created_at"
