b3ce8cf837dd6e7ebcb00ab389e6d30c8f478ce5
Max Plomer <maxplomer@gmail.com>
2015-05-06 23:36:18 -0400

    add a new comment

--- public/app.js
+++ public/app.js
@@ -77,7 +77,18 @@ angular.module('carbonCalculator', ['ui.router'])
     '$stateParams',
     'posts',
     function($scope, $stateParams, posts) {
+    	
       $scope.post = posts.posts[$stateParams.id];
+
+      $scope.addComment = function(){
+        if($scope.body === '') { return; }
+        $scope.post.comments.push({
+          body: $scope.body,
+          author: 'user',
+          upvotes: 0
+        });
+        $scope.body = '';
+      };
 
     }
   ]);
