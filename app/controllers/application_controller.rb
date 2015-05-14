2eda3eaf9e3d9fb49beb7dfd50d340d903834306
Max Plomer <maxplomer@gmail.com>
2015-05-14 10:42:19 -0400

    user auth works

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,12 @@ class ApplicationController < ActionController::Base
 
   respond_to :json
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
+
 end
