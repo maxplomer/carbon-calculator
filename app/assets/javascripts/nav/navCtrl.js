ef71101379f1d739cf97583db10eccea1ea29159
Max Plomer <maxplomer@gmail.com>
2015-05-13 22:16:22 -0400

    set $scope.user when the controller loads

--- app/assets/javascripts/nav/navCtrl.js
+++ app/assets/javascripts/nav/navCtrl.js
@@ -1,7 +1,12 @@
-angular.module('flapperNews')
+angular.module('carbonCalculator')
   .controller('NavCtrl', [
     '$scope',
     'Auth',
     function($scope, Auth){
+      $scope.signedIn = Auth.isAuthenticated;
+      $scope.logout = Auth.logout;
 
+      Auth.currentUser().then(function (user){
+        $scope.user = user;
+      });
     }]);
