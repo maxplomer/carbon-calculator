9e29475eb14ad30203e29b39abf3375fc312a695
Max Plomer <maxplomer@gmail.com>
2015-05-16 17:47:56 -0400

    create co2_output model function

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
+  def co2_output
+    self.gal_of_gas_per_day + gal_of_hotwater_per_day
+  end
+
   def as_json(options = {})
     super(options.merge(include: [:user]))
   end
