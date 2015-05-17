45fd6b00dcc8948639027d2534ac50b68ed2f090
Max Plomer <maxplomer@gmail.com>
2015-05-17 01:07:47 -0400

    don't need as_json method in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,8 +4,4 @@ class Post < ActiveRecord::Base
   def co2_output
     self.gal_of_gas_per_day + gal_of_hotwater_per_day + kwh_of_energy_per_day
   end
-
-  def as_json(options = {})
-    super(options.merge(include: [:user]))
-  end
 end
