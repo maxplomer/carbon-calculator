c845f40c13dfa4992ad5a5e5f590eece689a2ba8
Max Plomer <maxplomer@gmail.com>
2015-05-18 22:27:37 -0400

    formatted_created_at

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,11 @@ class Post < ActiveRecord::Base
   def co2_output
     self.gal_of_gas_per_day + gal_of_hotwater_per_day + kwh_of_energy_per_day
   end
+
+  def formatted_created_at
+    self
+      .created_at
+      .in_time_zone("Eastern Time (US & Canada)")
+      .strftime("%m/%d/%Y")
+  end
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,7 +1,7 @@
 class PostSerializer < ActiveModel::Serializer
 
   attributes :id, :gal_of_gas_per_day, :gal_of_hotwater_per_day, 
-             :kwh_of_energy_per_day, :co2_output
+             :kwh_of_energy_per_day, :co2_output, :formatted_created_at
 
   has_one :user
 
