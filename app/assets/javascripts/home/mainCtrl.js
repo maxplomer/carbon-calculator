a33e40e87625209858b7f1aaa378e49500b86f2a
Max Plomer <maxplomer@gmail.com>
2015-05-18 23:03:11 -0400

    sends hotwater_source to ruby

--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -12,11 +12,13 @@ angular.module('carbonCalculator')
         posts.create({
           gal_of_gas_per_day: $scope.gal_of_gas_per_day,
           gal_of_hotwater_per_day: $scope.gal_of_hotwater_per_day,
+          hotwater_source: $scope.hotwater_source,
           kwh_of_energy_per_day: $scope.kwh_of_energy_per_day
         });
 
         $scope.gal_of_gas_per_day = '';
         $scope.gal_of_hotwater_per_day = '';
+        $scope.hotwater_source = '';
         $scope.kwh_of_energy_per_day = '';
       };
 
