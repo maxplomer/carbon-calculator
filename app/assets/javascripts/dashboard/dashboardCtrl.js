848b9a2a6ad1900c72f784f10c8f8320ab8c083b
Max Plomer <maxplomer@gmail.com>
2015-06-01 23:35:49 -0400

    remove what not using in dashboard controller

--- app/assets/javascripts/dashboard/dashboardCtrl.js
+++ app/assets/javascripts/dashboard/dashboardCtrl.js
@@ -18,47 +18,5 @@ angular.module('carbonCalculator')
         $scope.user = user;
       });
 
-
-
-
-
-      $scope.posts = posts.posts;
-
-      $scope.sortMethod = '-id';
-
-      $scope.addPost = function(){
-        posts.create({
-          gal_of_gas_per_day: $scope.gal_of_gas_per_day,
-          gal_of_hotwater_per_day: $scope.gal_of_hotwater_per_day,
-          hotwater_source: $scope.hotwater_source,
-          kwh_of_energy_per_day: $scope.kwh_of_energy_per_day,
-          energy_source: $scope.energy_source,
-          lbs_of_meat_per_day: $scope.lbs_of_meat_per_day,
-          airline_miles_per_year: $scope.airline_miles_per_year
-        });
-
-        $scope.gal_of_gas_per_day = '';
-        $scope.gal_of_hotwater_per_day = '';
-        $scope.hotwater_source = '';
-        $scope.kwh_of_energy_per_day = '';
-        $scope.energy_source = '';
-        $scope.lbs_of_meat_per_day = '';
-        $scope.airline_miles_per_year = '';
-      };
-
-      $scope.login = function() {
-        Auth.login($scope.user).then(function() {
-          $state.go('home');
-        });
-      };
-
-      $scope.register = function() {
-        Auth.register($scope.user).then(function() {
-          $state.go('home');
-        });
-      };
-
-
-
     }
   ]);
