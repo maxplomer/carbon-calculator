68c8d54316a80bbb324ad38ab837bbf8792374fb
Max Plomer <maxplomer@gmail.com>
2015-06-04 15:29:49 -0400

    move new footprint form to dashboard

--- app/assets/javascripts/dashboard/dashboardCtrl.js
+++ app/assets/javascripts/dashboard/dashboardCtrl.js
@@ -6,6 +6,28 @@ angular.module('carbonCalculator')
     'posts',
     function($scope, $state, Auth, posts) {
 
+      $scope.addPost = function(){
+        posts.create({
+          gal_of_gas_per_day: $scope.gal_of_gas_per_day,
+          gal_of_hotwater_per_day: $scope.gal_of_hotwater_per_day,
+          hotwater_source: $scope.hotwater_source,
+          kwh_of_energy_per_day: $scope.kwh_of_energy_per_day,
+          energy_source: $scope.energy_source,
+          lbs_of_meat_per_day: $scope.lbs_of_meat_per_day,
+          airline_miles_per_year: $scope.airline_miles_per_year
+        });
+
+        $scope.gal_of_gas_per_day = '';
+        $scope.gal_of_hotwater_per_day = '';
+        $scope.hotwater_source = '';
+        $scope.kwh_of_energy_per_day = '';
+        $scope.energy_source = '';
+        $scope.lbs_of_meat_per_day = '';
+        $scope.airline_miles_per_year = '';
+      };
+
+      //Auth
+
       $scope.logout = function() {
         Auth.logout().then(function() {
           $state.go('home');
--- app/assets/javascripts/home/mainCtrl.js
+++ app/assets/javascripts/home/mainCtrl.js
@@ -55,27 +55,6 @@ angular.module('carbonCalculator')
       //call changeSortMethod() here to set the sort variables
       $scope.changeSortMethod('-co2_output', true); //flag=true will force descending
 
-
-      $scope.addPost = function(){
-        posts.create({
-          gal_of_gas_per_day: $scope.gal_of_gas_per_day,
-          gal_of_hotwater_per_day: $scope.gal_of_hotwater_per_day,
-          hotwater_source: $scope.hotwater_source,
-          kwh_of_energy_per_day: $scope.kwh_of_energy_per_day,
-          energy_source: $scope.energy_source,
-          lbs_of_meat_per_day: $scope.lbs_of_meat_per_day,
-          airline_miles_per_year: $scope.airline_miles_per_year
-        });
-
-        $scope.gal_of_gas_per_day = '';
-        $scope.gal_of_hotwater_per_day = '';
-        $scope.hotwater_source = '';
-        $scope.kwh_of_energy_per_day = '';
-        $scope.energy_source = '';
-        $scope.lbs_of_meat_per_day = '';
-        $scope.airline_miles_per_year = '';
-      };
-
       //Auth
 
       $scope.login = function() {
