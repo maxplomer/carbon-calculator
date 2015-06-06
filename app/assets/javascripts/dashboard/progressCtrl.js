dbceb6a420a1ef118dc0060bec680d4c64fc0d15
Max Plomer <maxplomer@gmail.com>
2015-06-05 22:18:53 -0400

    add angular chart

--- app/assets/javascripts/dashboard/progressCtrl.js
+++ app/assets/javascripts/dashboard/progressCtrl.js
@@ -1,4 +1,4 @@
-angular.module('carbonCalculator')
+angular.module('carbonCalculator', ["chart.js"])
   .controller('ProgressCtrl', [
     '$scope',
     '$state',
@@ -8,5 +8,16 @@ angular.module('carbonCalculator')
 
       $scope.posts = posts.posts;
 
+      $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+      $scope.series = ['Series A', 'Series B'];
+      $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+      ];
+      $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+      };
+
     }
   ]);
+
