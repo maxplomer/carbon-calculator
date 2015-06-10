6d40ec2db1e796c9d6363927cf27f05d0f0030ee
Max Plomer <maxplomer@gmail.com>
2015-06-10 15:04:09 -0400

    add log scale feature, need scatterplot

--- app/assets/javascripts/dashboard/progressCtrl.js
+++ app/assets/javascripts/dashboard/progressCtrl.js
@@ -6,22 +6,36 @@ angular.module('carbonCalculator')
     'posts',
     function($scope, $state, Auth, posts) {
 
+      $scope.logScale = true;
+
       $scope.footprints = posts.footprints;
 
-      $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-      $scope.series = ['Daily Average', 'Series B'];
-      $scope.data = [
-        [65, 59, 80, 81, 56, 55, 40],
-        [28, 48, 40, 19, 86, 27, 90]
-      ];
+      //$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+      $scope.series = ['Footprints'];
+      //$scope.data = [[65, 59, 80, 81, 56, 55, 40]];
 
-      $scope.myData = [];
-      $scope.myDates = [];
-      angular.forEach($scope.footprints, function(footprint) {
-        $scope.myData.push(footprint.co2_output)
-        $scope.myDates.push(footprint.created_at)
-      });
 
+
+      $scope.setGraphData = function() {      
+        $scope.myData = [];
+        $scope.myDates = [];
+        $scope.myLabels = [];
+
+        angular.forEach($scope.footprints, function(footprint) {
+          var c = footprint.co2_output;
+          if ($scope.logScale) { c = Math.log(c) }
+          $scope.myData.push(Math.round(c));
+          var d = new Date(footprint.created_at);
+          $scope.myDates.push(d);
+          $scope.myLabels.push("label");
+        });
+
+        $scope.data = [$scope.myData]
+        $scope.labels = $scope.myLabels
+      };
+      $scope.setGraphData();
+
+      
 
       $scope.onClick = function (points, evt) {
         console.log(points, evt);
