a8acb58df97ce68efaa9818772aacb298a08a5bb
Max Plomer <maxplomer@gmail.com>
2015-06-10 14:17:28 -0400

    output dates

--- app/assets/javascripts/dashboard/progressCtrl.js
+++ app/assets/javascripts/dashboard/progressCtrl.js
@@ -14,6 +14,15 @@ angular.module('carbonCalculator')
         [65, 59, 80, 81, 56, 55, 40],
         [28, 48, 40, 19, 86, 27, 90]
       ];
+
+      $scope.myData = [];
+      $scope.myDates = [];
+      angular.forEach($scope.footprints, function(footprint) {
+        $scope.myData.push(footprint.co2_output)
+        $scope.myDates.push(footprint.created_at)
+      });
+
+
       $scope.onClick = function (points, evt) {
         console.log(points, evt);
       };
--- app/serializers/footprint_serializer.rb
+++ app/serializers/footprint_serializer.rb
@@ -1,7 +1,8 @@
 class FootprintSerializer < ActiveModel::Serializer
 
   attributes :id, :energy_source, :carbon_sources, :co2_output, 
-             :formatted_created_at, :formatted_updated_at, :updated_at_id
+             :formatted_created_at, :formatted_updated_at, :updated_at_id,
+             :created_at
 
   has_one :user
 
