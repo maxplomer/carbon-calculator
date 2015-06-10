3a36bd2b035063687c0be5bf399a57442e12bcb1
Max Plomer <maxplomer@gmail.com>
2015-06-10 15:10:52 -0400

    change series name

--- app/assets/javascripts/dashboard/progressCtrl.js
+++ app/assets/javascripts/dashboard/progressCtrl.js
@@ -11,7 +11,7 @@ angular.module('carbonCalculator')
       $scope.footprints = posts.footprints;
 
       //$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-      $scope.series = ['Footprints'];
+      $scope.series = ['Daily Average Footprints'];
       //$scope.data = [[65, 59, 80, 81, 56, 55, 40]];
 
 
