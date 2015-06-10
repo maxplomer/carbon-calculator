9d75f3dd690f2e56129d329fb7cd1db0ab375472
Max Plomer <maxplomer@gmail.com>
2015-06-10 17:05:59 -0400

    trying scatter plot

--- app/assets/javascripts/dashboard/progressCtrl.js
+++ app/assets/javascripts/dashboard/progressCtrl.js
@@ -14,8 +14,6 @@ angular.module('carbonCalculator')
       $scope.series = ['Daily Average Footprints'];
       //$scope.data = [[65, 59, 80, 81, 56, 55, 40]];
 
-
-
       $scope.setGraphData = function() {      
         $scope.myData = [];
         $scope.myDates = [];
@@ -35,12 +33,46 @@ angular.module('carbonCalculator')
       };
       $scope.setGraphData();
 
-      
-
       $scope.onClick = function (points, evt) {
         console.log(points, evt);
       };
 
+      var data2 = [
+        {
+          label: 'My First dataset',
+          strokeColor: '#F16220',
+          pointColor: '#F16220',
+          pointStrokeColor: '#fff',
+          data: [
+            { x: 19, y: 65 }, 
+            { x: 27, y: 59 }, 
+            { x: 28, y: 69 }, 
+            { x: 40, y: 81 },
+            { x: 48, y: 56 }
+          ]
+        },
+        {
+          label: 'My Second dataset',
+          strokeColor: '#007ACC',
+          pointColor: '#007ACC',
+          pointStrokeColor: '#fff',
+          data: [
+            { x: 19, y: 75 }, 
+            { x: 27, y: 69 }, 
+            { x: 28, y: 70 }, 
+            { x: 40, y: 31 },
+            { x: 48, y: 76 }, 
+            { x: 52, y: 23 }, 
+            { x: 24, y: 32 }
+          ]
+        }
+      ];
+      var options = {};
+
+      // Get the context of the canvas element we want to select
+      var ctx = document.getElementById("myChart").getContext("2d");
+      var mychart = new Chart(ctx).Scatter(data2, options);
+      
     }
   ]);
 
