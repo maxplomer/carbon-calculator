d2b80b0e3ae47cb101b75048d1dfe54d2b7a0a30
Max Plomer <maxplomer@gmail.com>
2015-06-11 00:31:09 -0400

    Revert "ready to start switching to new graphing directive"
    This reverts commit 3c7cb99e1bbac3446e5a1a924e326be549010ea9.

--- app/assets/javascripts/dashboard/progressCtrl.js
+++ app/assets/javascripts/dashboard/progressCtrl.js
@@ -6,79 +6,79 @@ angular.module('carbonCalculator')
     'posts',
     function($scope, $state, Auth, posts) {
 
-      // //scatterplot graph
+      //scatterplot graph
 
-      // var getData = function() {
-      //   var result = [{
-      //           x: new Date('2011-04-11T17:00:00'),
-      //           y: 25
-      //         }];
+      var getData = function() {
+        var result = [{
+                x: new Date('2011-04-11T17:00:00'),
+                y: 25
+              }];
 
-      //   angular.forEach(posts.footprints, function(footprint) {
-      //     result.unshift({
-      //       x: new Date(footprint.created_at),
-      //       y: Math.floor(footprint.co2_output / 1000)
-      //     })
-      //   });
+        angular.forEach(posts.footprints, function(footprint) {
+          result.unshift({
+            x: new Date(footprint.created_at),
+            y: Math.floor(footprint.co2_output / 1000)
+          })
+        });
 
-      //   return result;
-      // };
+        return result;
+      };
 
-      // var drawScatter = function() {
-      //   Chart.defaults.global.responsive = true;
-      //   Chart.defaults.global.animation = false;
+      var drawScatter = function() {
+        Chart.defaults.global.responsive = true;
+        Chart.defaults.global.animation = false;
 
 
 
-      //   var data3 = [
-      //     {
-      //       label: 'daily average footprint',
-      //       strokeColor: '#A31515',
-      //       data: getData()
-      //     }];
+        var data3 = [
+          {
+            label: 'daily average footprint',
+            strokeColor: '#A31515',
+            data: getData()
+          }];
 
-      //   var ctx2 = document.getElementById("myChartWithDates").getContext("2d");
-      //   var myDateLineChart = new Chart(ctx2).Scatter(data3, {
-      //     bezierCurve: true,
-      //     showTooltips: true,
-      //     scaleShowHorizontalLines: true,
-      //     scaleShowLabels: true,
-      //     scaleType: "date",
-      //     scaleLabel: "<%=value%> kg"
-      //   });
+        var ctx2 = document.getElementById("myChartWithDates").getContext("2d");
+        var myDateLineChart = new Chart(ctx2).Scatter(data3, {
+          bezierCurve: true,
+          showTooltips: true,
+          scaleShowHorizontalLines: true,
+          scaleShowLabels: true,
+          scaleType: "date",
+          scaleLabel: "<%=value%> kg"
+        });
 
-      // };
-      // drawScatter();
+      };
+      drawScatter();
 
-      // //angular charts graph
-      // $scope.logScale = true;
+      //angular charts graph
+      $scope.logScale = true;
 
-      // //$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-      // $scope.series = ['Daily Average Footprints'];
-      // //$scope.data = [[65, 59, 80, 81, 56, 55, 40]];
+      //$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+      $scope.series = ['Daily Average Footprints'];
+      //$scope.data = [[65, 59, 80, 81, 56, 55, 40]];
 
-      // $scope.setGraphData = function() {      
-      //   $scope.myData = [];
-      //   $scope.myDates = [];
-      //   $scope.myLabels = [];
+      $scope.setGraphData = function() {      
+        $scope.myData = [];
+        $scope.myDates = [];
+        $scope.myLabels = [];
 
-      //   angular.forEach($scope.footprints, function(footprint) {
-      //     var c = footprint.co2_output;
-      //     if ($scope.logScale) { c = Math.log(c) }
-      //     $scope.myData.unshift(Math.round(c));
-      //     var d = new Date(footprint.created_at);
-      //     $scope.myDates.unshift(d);
-      //     $scope.myLabels.unshift("label");
-      //   });
+        angular.forEach($scope.footprints, function(footprint) {
+          var c = footprint.co2_output;
+          if ($scope.logScale) { c = Math.log(c) }
+          $scope.myData.unshift(Math.round(c));
+          var d = new Date(footprint.created_at);
+          $scope.myDates.unshift(d);
+          $scope.myLabels.unshift("label");
+        });
 
-      //   $scope.data = [$scope.myData]
-      //   $scope.labels = $scope.myLabels
-      // };
-      // $scope.setGraphData();
+        $scope.data = [$scope.myData]
+        $scope.labels = $scope.myLabels
+      };
+      $scope.setGraphData();
 
-      // $scope.onClick = function (points, evt) {
-      //   console.log(points, evt);
-      // };
+      $scope.onClick = function (points, evt) {
+        console.log(points, evt);
+      };
 
     }
   ]);
