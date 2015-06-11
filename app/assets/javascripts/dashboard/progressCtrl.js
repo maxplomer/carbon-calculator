8e4d4b3d645e705dcffce2cf07129f69705330a9
Max Plomer <maxplomer@gmail.com>
2015-06-11 00:49:40 -0400

    remove other graph

--- app/assets/javascripts/dashboard/progressCtrl.js
+++ app/assets/javascripts/dashboard/progressCtrl.js
@@ -6,8 +6,6 @@ angular.module('carbonCalculator')
     'posts',
     function($scope, $state, Auth, posts) {
 
-      $scope.footprints = posts.footprints;
-
       //scatterplot graph
 
       var getData = function() {
@@ -27,9 +25,7 @@ angular.module('carbonCalculator')
         Chart.defaults.global.responsive = true;
         Chart.defaults.global.animation = false;
 
-
-
-        var data3 = [
+        var data = [
           {
             label: 'daily average footprint',
             strokeColor: '#A31515',
@@ -37,47 +33,17 @@ angular.module('carbonCalculator')
           }];
 
         var ctx2 = document.getElementById("myChartWithDates").getContext("2d");
-        var myDateLineChart = new Chart(ctx2).Scatter(data3, {
+        var myDateLineChart = new Chart(ctx2).Scatter(data, {
           bezierCurve: true,
           showTooltips: true,
           scaleShowHorizontalLines: true,
-          scaleShowLabels: false,
+
           scaleType: "date",
-          scaleLabel: "<%=value%> kg"
+          scaleLabel: "<%=value%> g"
         });
 
       };
       drawScatter();
-
-      //angular charts graph
-      $scope.logScale = true;
-
-      //$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-      $scope.series = ['Daily Average Footprints'];
-      //$scope.data = [[65, 59, 80, 81, 56, 55, 40]];
-
-      $scope.setGraphData = function() {      
-        $scope.myData = [];
-        $scope.myDates = [];
-        $scope.myLabels = [];
-
-        angular.forEach($scope.footprints, function(footprint) {
-          var c = footprint.co2_output;
-          if ($scope.logScale) { c = Math.log(c) }
-          $scope.myData.unshift(Math.round(c));
-          var d = new Date(footprint.created_at);
-          $scope.myDates.unshift(d);
-          $scope.myLabels.unshift("label");
-        });
-
-        $scope.data = [$scope.myData]
-        $scope.labels = $scope.myLabels
-      };
-      $scope.setGraphData();
-
-      $scope.onClick = function (points, evt) {
-        console.log(points, evt);
-      };
 
     }
   ]);
